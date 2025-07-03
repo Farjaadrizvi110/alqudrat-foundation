eb87688dccca47900e1b5052c59d474b19506c6a
Farjaadrizvi110 <farjaadrizvi@gmail.com>
2025-07-03 19:10:00 +0500

    Add next.config.js 1751551800976

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ['www.alqudrat.org.uk'],
+    unoptimized: true
+  },
+  reactStrictMode: true,
+}
+
+module.exports = nextConfig
